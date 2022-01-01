94d9e61c18ef5da2cc11478e94a1e6e9faa85776
Tuba Adıgüzel <71885903+tubaAdgzl@users.noreply.github.com>
2022-01-01 09:28:38 +0300

    update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static("public"))
 app.use("/",pageRoute);
 app.use("/photos",photoRoute);
 
-const PORT  = 3000;
+const PORT  = process.env.PORT || 5000;
 app.listen(PORT,()=>{
 	console.log(`Sunucu ${PORT} portunda başlatıldı`)
 })
